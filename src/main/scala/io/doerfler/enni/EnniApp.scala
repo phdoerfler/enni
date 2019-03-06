0a748239bdc7d34c89b80dfbdad6baaad33d262f
Philipp Dörfler <ph@doerfler.io>
2019-03-06 21:14:33 +0100

    Added multiline support

--- src/main/scala/io/doerfler/enni/EnniApp.scala
+++ src/main/scala/io/doerfler/enni/EnniApp.scala
@@ -5,10 +5,14 @@ import org.parboiled2._
 import scala.io.Source
 
 object EnniApp extends App {
-  val s = Source.stdin.getLines.mkString("\n")
-  implicit val parser = new EnvelopeParser(s)
-  val res = parser.EnvelopeInput.run().toEither.left.map {
-    case e: ParseError => parser.formatError(e, new ErrorFormatter(showTraces = true))
+  for {
+    s <- Source.stdin.getLines
+  } {
+    implicit val parser = new EnvelopeParser(s)
+    val res = parser.EnvelopeInput.run().toEither.left.map {
+      case e: ParseError => parser.formatError(e, new ErrorFormatter(showTraces = true))
+    }
+    println(res)
+    println("===========================")
   }
-  println(res)
 }
